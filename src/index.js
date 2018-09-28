0f0c1c1805ce8b7b08b488ab1411ce1f113d3e37
Dezman66 <dezman66@gmail.com>
2018-09-28 16:18:42 +0300

    Dezman66

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+    const arr1 = equation.split(" * ");
+    const arr2 = arr1[1].split("^2 ");
+    const arr3 = equation.split(" * x ");
+    
+    const a = eval(arr1[0]);
+    const b = eval(arr2[1]);
+    const c = eval(arr3[1]);
+  
+    let x1 = Math.round((-b-Math.sqrt(b*b-4*a*c))/(2*a));
+    let x2 = Math.round((-b+Math.sqrt(b*b-4*a*c))/(2*a));
+    let ans = [x1, x2]
+    
+    function sortNumber(m,n) {
+      return m - n;
+  }
+
+
+    return ans.sort(sortNumber);// your implementation
 }
+
